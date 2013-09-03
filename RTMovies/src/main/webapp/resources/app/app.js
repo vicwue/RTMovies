e0b94e845e236faecd370992a89afed7a2c7140e
Victor Wuendisch <vicwue@gmail.com>
2013-09-03 03:26:34 +0200

    d3 app mock

--- RTMovies/src/main/webapp/resources/app/app.js
+++ RTMovies/src/main/webapp/resources/app/app.js
@@ -1,68 +1,7 @@
-sap.ui.core.Control.extend("myHTML", { // call the new Control type "my.Hello"
-	// and let it inherit from sap.ui.core.Control
-	metadata: { // the Control API
-		properties: {
-			// setter and getter are created behind the scenes,
-			// including data binding and type validation
-			id: "string",
-			title: "string",
-			// year: "int",
-			// mpaa_rating: "string",
-			// runtime: "int",
-			// critics_consensus: "string",
-			// release_dates: {
-			// theater: "string"
-			// },
 
-			critics_rating: "string",
-			critics_score: "int",
-			audience_rating: "string",
-			audience_score: "int",
-		// posters: {
-		// thumbnail: "string",
-		// profile: "string",
-		// detailed: "string",
-		// original: "string"
-		// },
-		// abridged_cast: [ {
-		// name: "string",
-		// id: "string",
-		// characters: [ "string" ]
-		// } ],
-		// synopsis: "string",
-
-		// links: {
-		// self: "http://api.rottentomatoes.com/api/public/v1.0/movies/771193517.json",
-		// alternate: "http://www.rottentomatoes.com/m/the_wolverine_2012/",
-		// cast: "http://api.rottentomatoes.com/api/public/v1.0/movies/771193517/cast.json",
-		// clips: "http://api.rottentomatoes.com/api/public/v1.0/movies/771193517/clips.json",
-		// reviews: "http://api.rottentomatoes.com/api/public/v1.0/movies/771193517/reviews.json",
-		// similar: "http://api.rottentomatoes.com/api/public/v1.0/movies/771193517/similar.json"
-		// }
-
-		}
-	},
-
-	renderer: function(oRm, oControl) { // the part creating the HTML
-		oRm.write("<div><h1>");
-		oRm.writeEscaped(oControl.getTitle()); // write the Control property 'name', with XSS protection
-		oRm.write("</h1><div>");
-		oRm.writeEscaped(oControl.getCritics_score());
-		oRm.write("</div></div>");
-	}
-});
-
-var oModel = new sap.ui.model.json.JSONModel();
-
-var oHTML = new myHTML();
-oHTML.setModel(oModel);
-oHTML.bindProperty("title", "data/movies/1/title");
-oHTML.bindProperty("critics_score", "data/movies/1/ratings/critics_score");
 
 // STEP 1: create the Table control
 var oTable = new sap.ui.table.Table();
-oTable.setModel(oModel);
-oTable.bindRows("/data/movies"); // bind the rows against the "data"
 
 // define the two Table columns
 oTable.addColumn(new sap.ui.table.Column("name", {
@@ -88,9 +27,12 @@ oTable.addColumn(new sap.ui.table.Column("id", {
 
 // finally place the Table into the UI
 oTable.placeAt("content");
-oHTML.placeAt("content");
+//oHTML.placeAt("content");
 
 // STEP 2: create a JSONModel and bind the Table to this model
+var oModel = new sap.ui.model.json.JSONModel();
+oTable.setModel(oModel);
+oTable.bindRows("/data/movies"); // bind the rows against the "data"
 
 // STEP 3: load the data
 jQuery.ajax({
@@ -105,3 +47,6 @@ jQuery.ajax({
 		alert("error occurred");
 	}
 });
+
+
+
---
+++ RTMovies/src/main/webapp/resources/app/appd3.js
@@ -0,0 +1,46 @@
+var margin = {top: 40, right: 0, bottom: 0, left: 0},
+    width = document.width - margin.left - margin.right,
+    height = document.height - margin.top - margin.bottom;
+
+var color = d3.scale.category20c();
+
+var treemap = d3.layout.treemap()
+    .size([width, height])
+    .sticky(true)
+    .value(function(d) { return d.size; });
+
+var div = d3.select("body").append("div")
+    .style("position", "relative")
+    .style("width", (width + margin.left + margin.right) + "px")
+    .style("height", (height + margin.top + margin.bottom) + "px")
+    .style("left", margin.left + "px")
+    .style("top", margin.top + "px");
+
+d3.json("test.json", function(error, root) {
+  var node = div.datum(root).selectAll(".node")
+      .data(treemap.nodes)
+    .enter().append("div")
+      .attr("class", "node")
+      .call(position)
+      .style("background-image", function(d) { return "url("+d.thumbnail+")"; })
+      .text(function(d) { return d.children ? null : d.name; });
+
+  d3.selectAll("input").on("change", function change() {
+    var value = this.value === "count"
+        ? function() { return 1; }
+        : function(d) { return d.size; };
+
+    node
+        .data(treemap.value(value).nodes)
+      .transition()
+        .duration(1500)
+        .call(position);
+  });
+});
+
+function position() {
+  this.style("left", function(d) { return d.x + "px"; })
+      .style("top", function(d) { return d.y + "px"; })
+      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}
